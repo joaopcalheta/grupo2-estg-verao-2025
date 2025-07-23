b23c6acda41f77d4c7e6be29ac60c472b1810b3d
AfonsoSousaDEV <04afonsosousa@gmail.com>
2025-07-23 14:59:53 +0100

    comentarios so

--- client/public/js/clientProfile.js
+++ client/public/js/clientProfile.js
@@ -1,7 +1,8 @@
+//clicar para chegar ao meu perfil
 function clientProfile() {
     location.href = '/myProfileClient'
 }
-
+//clicar para chegar as candidaturas
 function clientCand() {
     location.href = '/myCandClient'
 }
