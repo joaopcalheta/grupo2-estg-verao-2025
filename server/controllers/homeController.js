537018024f2d9f6fac1956cf0566d23ee33acc5a
MatildeAbreu <187966539+MIBA6@users.noreply.github.com>
2025-07-28 14:25:53 +0100

    Anuncios dinamicos na página home

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,11 +1,17 @@
+const Announcement = require("../models/announcement");
+
 const getHome = async (req, res) => {
   try {
+    const announcements = await Announcement.find().sort({ createdAt: -1 });
     res.render("home", {
-      title: "",
+      title: "Página Inicial",
+      announcements, // passa os dados
     });
   } catch (err) {
-    console.error(err);
+    console.error("Erro ao carregar página inicial:", err);
+    res.status(500).send("Erro ao carregar página inicial");
   }
 };
 
 module.exports = { getHome };
+
