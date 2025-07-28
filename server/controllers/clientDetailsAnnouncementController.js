c072185ddec923f60fd9e97d7e6353dd50ffef00
MatildeAbreu <187966539+MIBA6@users.noreply.github.com>
2025-07-28 15:00:08 +0100

    Controller da página saber mais

--- server/controllers/clientDetailsAnnouncementController.js
+++ server/controllers/clientDetailsAnnouncementController.js
@@ -1,12 +1,37 @@
+const Announcement = require("../models/announcement");
+
 const getClientDetailsAnnouncement = async (req, res) => {
   try {
+    const announcementId = req.query.id;
+    if (!announcementId) {
+      return res.status(400).send("ID do anúncio não foi fornecido.");
+    }
+
+    const announcement = await Announcement.findById(announcementId);
+    if (!announcement) {
+      return res.status(404).send("Anúncio não encontrado.");
+    }
+
     res.render("client-details-announcement", {
-      title: "Candidaturas",
+      title: "Detalhes do Anúncio",
+      announcement
     });
   } catch (err) {
-    console.error("Erro ao carregar a página Candidaturas:", err);
+    console.error("Erro ao carregar a página Detalhes do Anúncio:", err);
     res.status(500).send("Erro interno no servidor");
   }
 };
 
-module.exports = { getClientDetailsAnnouncement };
+const postClientSubmitApplication = async (req, res) => {
+  try {
+    // Lógica da candidatura caso o utilizador esteje logado
+    res.send("Candidatura enviada!");
+  } catch (err) {
+    res.status(500).send("Erro ao enviar a candidatura");
+  }
+}
+
+module.exports = {
+  getClientDetailsAnnouncement,
+  postClientSubmitApplication,
+};
