c7d1c8971ed59bf81e462df05fa4653e9cd65f71
OUTL4W <67383837+OUTL4Wzin@users.noreply.github.com>
2025-07-29 13:50:10 +0100

    criei company-details-announcements

---
+++ server/controllers/companyDetailsAnnouncementController.js
@@ -0,0 +1,25 @@
+const Announcement = require("../models/announcement");
+
+const getCompanyDetailsAnnouncement = async (req, res) => {
+  try {
+    const announcementId = req.query.id;
+    if (!announcementId) {
+      return res.status(400).send("ID do anúncio não foi fornecido.");
+    }
+
+    const announcement = await Announcement.findById(announcementId);
+    if (!announcement) {
+      return res.status(404).send("Anúncio não encontrado.");
+    }
+
+    res.render("company-details-announcement", {
+      title: "Detalhes do Anúncio",
+      announcement,
+    });
+  } catch (err) {
+    console.error("Erro ao carregar a página Detalhes do Anúncio:", err);
+    res.status(500).send("Erro interno no servidor");
+  }
+};
+
+module.exports = { getCompanyDetailsAnnouncement };
