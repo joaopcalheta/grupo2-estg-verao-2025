ed153efeed793f18074a7d83650e34d82b065d72
calheta <163058159+joaopcalheta@users.noreply.github.com>
2025-07-31 14:05:48 +0100

    menu my profile 4 submenus alteraceos de acordo com sr anselmo pediu

--- client/public/js/client-my-profile.js
+++ client/public/js/client-my-profile.js
@@ -2,7 +2,14 @@
 function clientProfile() {
   location.href = "/client-my-profile";
 }
+function profData() {
+  location.href = "/prof-data";
+}
 //clicar para chegar as candidaturas
 function clientCand() {
   location.href = "/client-my-applications";
+}
+
+function myComp() {
+  location.href = "/my-companies";
 }
