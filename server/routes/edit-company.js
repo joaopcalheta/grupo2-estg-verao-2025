395533a14357bd506062776b5351cb9e96daf2d8
OUTL4W <67383837+OUTL4Wzin@users.noreply.github.com>
2025-08-05 09:20:04 +0100

    botões de minhas empresas reparados

--- server/routes/edit-company.js
+++ server/routes/edit-company.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const editCompanyController = require("../controllers/edit-company");
 
-router.get("/edit-company", editCompanyController.getEditCompany); // renderiza a página
-router.post("/edit-company", editCompanyController.postEditCompany);
+router.get("/edit-company/:id", editCompanyController.getEditCompany); // renderiza a página
+router.post("/edit-company/:id", editCompanyController.postEditCompany);
 
 module.exports = router;
