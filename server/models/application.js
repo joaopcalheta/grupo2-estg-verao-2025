7b25d2ab58ebe5f133bf3eb642cc4257e66e519f
AfonsoSousaDEV <04afonsosousa@gmail.com>
2025-08-07 10:05:52 +0100

    Resolvido bug do nao logado nao poder enviar candidatura

--- server/models/application.js
+++ server/models/application.js
@@ -90,7 +90,6 @@ const applicationSchema = new mongoose.Schema(
     user_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
     },
     submittedAt: {
       type: Date,
